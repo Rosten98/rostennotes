1386de06538a76e54a917c2b8ee85476c640db8e
Gerardo <gerodel98@gmail.com>
2020-05-16 18:29:49 -0500

    Edited logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ class Navigation extends Component {
 	render() {
 		return (
 			<header className="navigation">
-				<p className="logo">Rosten<span className="rosten-notes">Notes</span></p>
+				<p className="logo">R<span className="rosten-notes">N</span></p>
 				<nav>
 				<a href="#">Inicio</a>
 				<a href="#">Notas</a>
