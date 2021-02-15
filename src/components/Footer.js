e85d095547e587e45c1dd69932fe316ece34e38e
Gerardo <gerodel98@gmail.com>
2021-02-14 20:01:51 -0600

    Last commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,18 @@ class Footer extends Component {
           Created and Designed by <a href="https://twitter.com/rosten98">@Rosten98</a>
         </p>
         <div className="icons wrapper">
-          <FaGithub className="icon"/>
-          <span className="desktop-display">Github</span>
-          <FaTwitter className="icon"/>
-          <span className="desktop-display">Twitter</span>
-          <FaPatreon className="icon"/>
-          <span className="desktop-display">Patreon</span>
+          <a href="https://github.com/rosten98" target="_blank">
+            <FaGithub className="icon"/>
+            <span className="desktop-display">Github</span>
+          </a>
+          <a href="https://twitter.com/rosten98" target="_blank">
+            <FaTwitter className="icon"/>
+            <span className="desktop-display">Twitter</span>
+          </a>
+          <a>
+            <FaPatreon className="icon"/>
+            <span className="desktop-display">Patreon</span>
+          </a>
         </div>         
       </footer>
     );
